e5dbe7efef34b2303afb74e1088dc20e2bbe7bed
aleksandergorecki80 <aleksandergorecki80@gmail.com>
2023-08-04 18:04:04 +0200

    TDD: Guessing game - start the project

--- Section_7__TestingCode/untitled/src/main/com/ag/calculator/Calculator.java
+++ Section_7__TestingCode/untitled/src/main/com/ag/calculator/Calculator.java
@@ -1,4 +1,8 @@
 package com.ag.calculator;
 
 public class Calculator {
+
+    public int add(int num1, int num2) {
+        return num1 + num2;
+    }
 }
--- Section_7__TestingCode/untitled/src/test/com/ag/calculator/CalculatorTest.java
+++ Section_7__TestingCode/untitled/src/test/com/ag/calculator/CalculatorTest.java
@@ -1,9 +1,47 @@
 package com.ag.calculator;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class CalculatorTest {
 
-    public void canAddZeroPlusZero(){
-        Calculator calc = new Calculator();
+    private Calculator calc;
+
+    @BeforeEach
+    void setUp() {
+        calc = new Calculator();
     }
 
+    @Test
+    public void canAddZeroPlusZero(){
+        int sum = calc.add(0, 0);
+        assertEquals(0, sum, "It expects the sum to be zero");
+    }
+
+    @Test
+    public void canAddOnePlusOne(){
+        int sum = calc.add(1, 1);
+        assertEquals(2, sum, "It expects the sum to be two");
+    }
+
+    @Test
+    public void canAddNegativeNumberToPositive(){
+        int sum = calc.add(-4, 1);
+        assertEquals(-3, sum, "It expects the sum to be minus three");
+    }
+
+    @Test
+    public void canAddTwoNegativesNumbers(){
+        int sum = calc.add(-3, -2);
+        assertEquals(-5, sum, "It expects the sum to be minus five");
+    }
+
+    @Test
+    public void canAddMaxIntPlusOne(){
+        int sum = calc.add(Integer.MAX_VALUE, 1);
+        System.out.println(sum);
+//        assertEquals(2147483648, sum, "It expects the sum to be 2147483648");
+    }
 }
